3075e9eace2ef8d06e142925439698f1f75b245c
sadicangel <sadicangel@hotmail.com>
2025-04-13 01:18:13 +0100

    Handle references. (#1)

--- src/AvroSchemaGenerator/AvroSchemaGenerator.cs
+++ src/AvroSchemaGenerator/AvroSchemaGenerator.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Frozen;
-using System.Reflection;
+﻿using System.Reflection;
 using System.Text.Json;
 using System.Text.Json.Nodes;
 
@@ -7,160 +6,178 @@ namespace AvroSchemaGenerator;
 
 public sealed class AvroSchemaGenerator
 {
-    internal static readonly FrozenDictionary<Type, JsonNode> WellKnownMappings = new Dictionary<Type, JsonNode>()
+    public static string Generate<T>(JsonSerializerOptions? options = null) =>
+        Implementation.Generate(typeof(T)).ToJsonString(options ?? new() { WriteIndented = true });
+
+    public static string Generate(Type type, JsonSerializerOptions? options = null) =>
+        Implementation.Generate(type).ToJsonString(options);
+
+    private readonly record struct Implementation(Dictionary<Type, JsonNode?> Schemas)
     {
-        [typeof(bool)] = WellKnownSchemas.Boolean,
-        [typeof(int)] = WellKnownSchemas.Int,
-        [typeof(long)] = WellKnownSchemas.Long,
-        [typeof(float)] = WellKnownSchemas.Float,
-        [typeof(double)] = WellKnownSchemas.Double,
-        [typeof(byte[])] = WellKnownSchemas.Bytes,
-        [typeof(string)] = WellKnownSchemas.String,
-        [typeof(DateOnly)] = WellKnownSchemas.Date,
-        [typeof(TimeOnly)] = WellKnownSchemas.TimeMillis,
-        [typeof(DateTime)] = WellKnownSchemas.TimestampMillis,
-        [typeof(decimal)] = WellKnownSchemas.Decimal,
-
-    }.ToFrozenDictionary();
-
-    public static string GenerateSchema<T>(JsonSerializerOptions? options = null) =>
-        GenerateSchema(typeof(T)).ToJsonString(options ?? new() { WriteIndented = true });
-
-    public static string GenerateSchema(Type type, JsonSerializerOptions? options = null) =>
-        GenerateSchema(type).ToJsonString(options);
-
-    internal static JsonNode GenerateSchema(Type type)
-    {
-        ArgumentNullException.ThrowIfNull(type);
-
-        if (WellKnownMappings.TryGetValue(type, out var schema))
+        private static Dictionary<Type, JsonNode?> GetWellKnownSchemas() => new()
         {
-            return schema.DeepClone();
+            [typeof(bool)] = WellKnownSchemas.Boolean,
+            [typeof(int)] = WellKnownSchemas.Int,
+            [typeof(long)] = WellKnownSchemas.Long,
+            [typeof(float)] = WellKnownSchemas.Float,
+            [typeof(double)] = WellKnownSchemas.Double,
+            [typeof(byte[])] = WellKnownSchemas.Bytes,
+            [typeof(string)] = WellKnownSchemas.String,
+            [typeof(DateOnly)] = WellKnownSchemas.Date,
+            [typeof(TimeOnly)] = WellKnownSchemas.TimeMillis,
+            [typeof(DateTime)] = WellKnownSchemas.TimestampMillis,
+            [typeof(decimal)] = WellKnownSchemas.Decimal,
+
+        };
+
+        public static JsonNode Generate(Type type) =>
+            new Implementation(GetWellKnownSchemas()).GenerateSchema(type);
+
+        private JsonNode GenerateSchema(Type type)
+        {
+            ArgumentNullException.ThrowIfNull(type);
+
+            if (Schemas.TryGetValue(type, out var schemaName))
+            {
+                if (schemaName is null)
+                {
+                    throw new InvalidOperationException($"Circular reference detected for type {type.FullName}");
+                }
+
+                return schemaName.DeepClone();
+            }
+
+            if (type.IsNullableValueType())
+            {
+                return GenerateUnionSchema(isNullable: true, [Nullable.GetUnderlyingType(type)!]);
+            }
+
+            if (type.IsMap())
+            {
+                return GenerateMapSchema(type.GetGenericArguments()[1]);
+            }
+
+            if (type.IsArray())
+            {
+                return GenerateArraySchema(type.GetElementType() ?? type.GetGenericArguments()[0]);
+            }
+
+            if (type.IsEnum)
+            {
+                return GenerateEnumSchema(type);
+            }
+
+            // TODO: Can we do "fixed"?
+
+            // TODO: We can do "error" by checking if it derives from Exception.
+
+            return GenerateRecordSchema(type);
         }
 
-        if (type.IsEnum)
+        private JsonObject GenerateMapSchema(Type valuesType)
         {
-            return GenerateEnum(type);
+            var map = JsonNode.Parse($$"""
+            {
+                "type": "map",
+                "values": {{GenerateSchema(valuesType)}}
+            }
+            """)!.AsObject();
+
+            return map;
         }
 
-        if (type.IsNullableValueType())
+        private JsonObject GenerateArraySchema(Type itemsType)
         {
-            return GenerateUnionSchema(isNullable: true, Nullable.GetUnderlyingType(type)!);
+            var array = JsonNode.Parse($$"""
+            {
+                "type": "array",
+                "items": {{GenerateSchema(itemsType)}}
+            }
+            """)!.AsObject();
+            return array;
         }
 
-        if (type.IsMap())
+        private JsonArray GenerateUnionSchema(bool isNullable, ReadOnlySpan<Type> types)
         {
-            return GenerateMapSchema(type.GetGenericArguments()[1]);
+            ArgumentOutOfRangeException.ThrowIfNegativeOrZero(types.Length);
+
+            var union = JsonNode.Parse(isNullable ? "[\"null\"]" : "[]")!.AsArray();
+            foreach (var type in types)
+            {
+                union.Add(GenerateSchema(type));
+            }
+
+            return union;
         }
 
-        if (type.IsArray())
+        private JsonObject GenerateEnumSchema(Type type)
         {
-            return GenerateArraySchema(type.GetElementType() ?? type.GetGenericArguments()[0]);
+            var @enum = JsonNode.Parse($$"""
+            {
+                "type": "enum",
+                "name": "{{type.Name}}",
+                "symbols": []
+            }
+            """)!.AsObject();
+
+            if (!string.IsNullOrEmpty(type.Namespace))
+            {
+                @enum["namespace"] = type.Namespace;
+            }
+
+            var symbols = @enum["symbols"]!.AsArray();
+            foreach (var symbol in Enum.GetNames(type))
+            {
+                symbols.Add(symbol);
+            }
+
+            Schemas[type] = @enum["name"];
+
+            return @enum;
         }
 
-        // TODO: Can we do "fixed"?
-
-        // TODO: We can do "error" by checking if it derives from Exception.
-
-        return GenerateRecordSchema(type);
-    }
-
-    private static JsonObject GenerateEnum(Type type)
-    {
-        var @enum = JsonNode.Parse($$"""
+        private JsonObject GenerateRecordSchema(Type type)
         {
-            "type": "enum",
-            "name": "{{type.Name}}",
-            "symbols": []
-        }
-        """)!.AsObject();
+            var record = JsonNode.Parse($$"""
+            {
+                "type": "record",
+                "name": "{{type.Name}}",
+                "fields": []
+            }
+            """)!.AsObject();
 
-        if (!string.IsNullOrEmpty(type.Namespace))
-        {
-            @enum["namespace"] = type.Namespace;
-        }
+            // We need to set the schema to null to avoid circular references.
+            Schemas[type] = null;
 
-        var symbols = @enum["symbols"]!.AsArray();
-        foreach (var symbol in Enum.GetNames(type))
-        {
-            symbols.Add(symbol);
-        }
+            if (!string.IsNullOrEmpty(type.Namespace))
+            {
+                record["namespace"] = type.Namespace;
+            }
 
-        return @enum;
-    }
-
-    private static JsonObject GenerateMapSchema(Type valuesType)
-    {
-        var map = JsonNode.Parse($$"""
-        {
-            "type": "map",
-            "values": {{GenerateSchema(valuesType)}}
-        }
-        """)!.AsObject();
-
-        return map;
-    }
-
-    private static JsonObject GenerateArraySchema(Type itemsType)
-    {
-        var array = JsonNode.Parse($$"""
-        {
-            "type": "array",
-            "items": {{GenerateSchema(itemsType)}}
-        }
-        """)!.AsObject();
-        return array;
-    }
-
-    private static JsonObject GenerateRecordSchema(Type type)
-    {
-        var record = JsonNode.Parse($$"""
-        {
-            "type": "record",
-            "name": "{{type.Name}}",
-            "fields": []
-        }
-        """)!.AsObject();
-
-        if (!string.IsNullOrEmpty(type.Namespace))
-        {
-            record["namespace"] = type.Namespace;
-        }
-
-        var nullabilityInfoContext = new NullabilityInfoContext();
-        var fields = record["fields"]!.AsArray();
-        foreach (var property in type.GetProperties())
-        {
-            var field = JsonNode.Parse($$"""
+            var nullabilityInfoContext = new NullabilityInfoContext();
+            var fields = record["fields"]!.AsArray();
+            foreach (var property in type.GetProperties())
+            {
+                var field = JsonNode.Parse($$"""
             {
                 "name": "{{property.Name}}"
             }
             """)!.AsObject();
 
-            var nullabilityInfo = nullabilityInfoContext.Create(property);
-            var isNullable = nullabilityInfo.WriteState == NullabilityState.Nullable;
-            var underlyingType = Nullable.GetUnderlyingType(property.PropertyType) ?? property.PropertyType;
+                var nullabilityInfo = nullabilityInfoContext.Create(property);
+                var isNullable = nullabilityInfo.WriteState == NullabilityState.Nullable;
+                var nonNullableType = Nullable.GetUnderlyingType(property.PropertyType) ?? property.PropertyType;
 
-            field["type"] = isNullable
-                ? GenerateUnionSchema(isNullable, underlyingType)
-                : GenerateSchema(property.PropertyType);
+                field["type"] = isNullable
+                    ? GenerateUnionSchema(isNullable, [nonNullableType])
+                    : GenerateSchema(nonNullableType);
 
-            fields.Add(field);
+                fields.Add(field);
+            }
+
+            Schemas[type] = record["name"];
+
+            return record;
         }
-
-        return record;
-    }
-
-    private static JsonArray GenerateUnionSchema(bool isNullable, params ReadOnlySpan<Type> types)
-    {
-        ArgumentOutOfRangeException.ThrowIfNegativeOrZero(types.Length);
-
-        var union = JsonNode.Parse(isNullable ? "[\"null\"]" : "[]")!.AsArray();
-        foreach (var type in types)
-        {
-            union.Add(GenerateSchema(type));
-        }
-
-        return union;
     }
 }
--- test/AvroSchemaGenerator.Tests/UnitTest1.cs
+++ test/AvroSchemaGenerator.Tests/UnitTest1.cs
@@ -2,20 +2,29 @@
 
 public class UnitTest1
 {
-    public record Test(
-        string Name,
-        int Age,
-        DateTime Date,
-        TimeOnly Time,
-        DateOnly DateOnly,
-        decimal Decimal,
-        bool? NullableBool,
-        string? NullableString);
-
     [Fact]
     public void Test1()
     {
-        var schema = AvroSchemaGenerator.GenerateSchema<Test>();
+        var schema = AvroSchemaGenerator.Generate<Test>();
         Assert.NotNull(schema);
     }
+}
+
+public record Test(
+    string Name,
+    int Age,
+    DateTime Date,
+    TimeOnly Time,
+    DateOnly DateOnly,
+    decimal Decimal,
+    bool? NullableBool,
+    string? NullableString,
+    TestEnum Enum1,
+    TestEnum Enum2);
+
+public enum TestEnum
+{
+    Value1,
+    Value2,
+    Value3,
 }
